4d9959cb26fb05701046bbdd8aff31fc0c38dcf2
SmokiPNG <119607566+SmokiPNG@users.noreply.github.com>
2022-12-25 18:32:43 +0300

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -7,7 +7,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
-	<title>Форма ds[kl</title>
+	<title>Форма входа</title>
 </head>
 <body>
             <form action="registration.php" method="post" align="center">
