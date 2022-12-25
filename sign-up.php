8915d966fd60077ca72c0a29707e2468c22bdb84
SmokiPNG <119607566+SmokiPNG@users.noreply.github.com>
2022-12-25 18:01:10 +0300

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -3,7 +3,9 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> dgg
+	fssd 
+	q43
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
