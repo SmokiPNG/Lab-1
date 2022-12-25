4e624563822b52f5be7592d7792ba8ac562af66c
SmokiPNG <119607566+SmokiPNG@users.noreply.github.com>
2022-12-25 18:31:35 +0300

    Update sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -4,10 +4,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	
+	
+	
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
 	<title>Форма входа</title>
+	
 </head>
 <body>
         <form action="auth.php" method="post" align="center">
